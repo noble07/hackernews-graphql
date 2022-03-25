285451a6a2df2bc07b5c75512e67a1714646897a
Juan Camilo Muñoz Zuleta <juancmz98@gmail.com>
2022-03-25 14:49:43 -0500

    Add: Apollo Server config

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { ApolloServer } from 'apollo-server'
+
+// 1. The schema object you created using Nexus defines your GraphQL schema. 
+// You need to provide this when instantiating your server since that’s how Apollo Server knows which API operations to support in the GraphQL API.
+import { schema } from './schema'
+export const server = new ApolloServer({
+  schema
+})
+
+const port = 3000
+
+// 2. You start the server and specify the port. After the server starts, it returns a url string inside a promise.
+server.listen({port}).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`)
+})
