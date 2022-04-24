34ed14612dfc698769a5ecf64f3661fa18386063
Juan Camilo Muñoz Zuleta <juancmz98@gmail.com>
2022-04-24 13:48:04 -0500

    Add: Filter for links

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -32,8 +32,20 @@ export const LinkQuery = extendType({ // 2. You are extending the Query root typ
       // 4. resolve is the name of the resolver function of the feed query. A resolver is the implementation for a GraphQL field.
       // Every field on each type (including the root types) has a resolver function which is executed to get the return value when fetching that type. For now, our resolver implementation is very simple, it just returns the links array.
       // The resolve function has four arguments, parent, args, context and info. We will get to these later.
-      resolve(_, __, { prisma }) {
-        return prisma.link.findMany()
+      args: {
+        filter: stringArg()
+      },
+      resolve(_, { filter }, { prisma }) {
+        const where = filter
+          ? {
+            OR: [
+              { description: { contains: filter } },
+              { url: { contains: filter } }
+            ]
+          }
+          : {}
+
+        return prisma.link.findMany({ where })
       }
     })
     t.field('link', {
